a74ad14be39369d2e2ae003d56fc870d362a6232
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2024-05-20 12:05:13 -0300

    agrege el de ejercicio-refactoring

---
+++ refactoring/ejercicio1-1.java
@@ -0,0 +1,15 @@
+/**
+* La clase Cliente tiene el siguiente protocolo. ¿Cómo puede mejorarlo?
+* Retorna el límite de crédito del cliente
+*/
+protected double lmtCrdt() {...}
+
+/**
+* Retorna el monto facturado al cliente desde la fecha f1 a la fecha f2
+*/
+protected double mtFcE(LocalDate f1, LocalDate f2) {...}
+
+/**
+* Retorna el monto cobrado al cliente desde la fecha f1 a la fecha f2
+*/
+protected double mtCbE(LocalDate f1, LocalDate f2) {...}
